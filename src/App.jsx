671051c01b0a2b68de14fb11e40764fab49329fd
beabritw <biaabrito88@gmail.com>
2025-06-03 22:09:07 -0300

    adicionando API de previsao do tempo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import PrevisaoClima from './components/PrevisaoClima';
+
+import './styles/PrevisaoClima.css'
+
+function App() {
+
+  return (
+    <>
+      <PrevisaoClima />
+    </>
+  )
+}
+
+export default App
